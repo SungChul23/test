02fbf7299b714081838ec3f35ac6cc98e6435232
사용자 junyoung011019 <kidjjy5@naver.com>
2023-11-21 23:15:00 +0900

    231121

--- 준영/display/display/main.cpp
+++ 준영/display/display/main.cpp
@@ -1,7 +1,25 @@
-#include <iostream>
+﻿#include <iostream>
 #include <windows.h>
-
+#include <stdlib.h>
+using namespace std;
 
 int main() {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	system("mode con: cols=100 lines=30");
+	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
+	
+	cout << "\n\n\n";
+	cout << " 	         	 ######   ######     ########     ###    ##    ## ##    ##                 \n";
+	cout << " 	         	##    ## ##    ##    ##     ##   ## ##   ###   ## ##   ##                  \n";
+	cout << " 	         	##       ##          ##     ##  ##   ##  ####  ## ##  ##                   \n";
+	cout << " 	         	##        ######     ########  ##     ## ## ## ## #####                    \n";
+	cout << " 	         	##             ##    ##     ## ######### ##  #### ##  ##                   \n";
+	SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN);
+	cout << " 	        	##    ## ##    ##    ##     ## ##     ## ##   ### ##   ##                  \n";
+	cout << " 	        	 ######   ######     ########  ##     ## ##    ## ##    ##                 \n";
+	cout << "\n\n\n\n";
+	cout << "                                  CS제일은행에 오신것을 환영합니다!                                 \n";
+	cout << "                                                                                                    \n";
 
 }
+

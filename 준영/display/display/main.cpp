99aa71d533f774dd67442a72e26dfc59bb5c0a21
사용자 junyoung011019 <kidjjy5@naver.com>
2023-11-22 09:42:37 +0900

    231122

--- 준영/display/display/main.cpp
+++ 준영/display/display/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
 	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	system("mode con: cols=100 lines=30");
+	system("mode con: cols=100 lines=35");
 	SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE);
 	
 	cout << "\n\n\n";
@@ -18,8 +18,24 @@ int main() {
 	cout << " 	        	##    ## ##    ##    ##     ## ##     ## ##   ### ##   ##                  \n";
 	cout << " 	        	 ######   ######     ########  ##     ## ##    ## ##    ##                 \n";
 	cout << "\n\n\n\n";
-	cout << "                                  CS제일은행에 오신것을 환영합니다!                                 \n";
-	cout << "                                                                                                    \n";
-
+	SetConsoleTextAttribute(hConsole, 15);
+	cout << "                                  CS제일은행에 오신것을 환영합니다!                                 \n\n\n\n";
+	cout << "                                 □□□□□□□□□□□□□□□□□□\n";
+	cout << "                                 □                                □\n";
+	cout << "                                 □        ";
+	SetConsoleTextAttribute(hConsole, 12);
+	cout << "기능을 입력하세요";
+	SetConsoleTextAttribute(hConsole, 15);
+	cout<<"       □\n";
+	cout << "                                 □                                □\n";
+	cout << "                                 □                                □\n";
+	cout << "                                 □           1. 로그인            □\n";
+	cout << "                                 □                                □\n";
+	cout << "                                 □          2. 회원가입           □\n";
+	cout << "                                 □                                □\n";
+	cout << "                                 □            3. 종료             □\n";
+	cout << "                                 □                                □\n";
+	cout << "                                 □                                □\n";
+	cout << "                                 □□□□□□□□□□□□□□□□□□\n";
 }
 
